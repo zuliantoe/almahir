f1a81a9bcd7127db5d5f46cdb8cd74113ddfa37e
Ghifari-s <ghifaripooh@gmail.com>
2026-02-07 09:09:30 +0700

    tambah controller pendaftaran: index,create,store

--- app/Modules/pendaftaran/Models/Pendaftaran.php
+++ app/Modules/pendaftaran/Models/Pendaftaran.php
@@ -7,8 +7,23 @@ class Pendaftaran extends Model
 {
     protected $table = 'pendaftaran';
 
+    protected $fillable = [
+        'nisn',
+        'nama_lengkap',
+        'tempat_lahir',
+        'tanggal_lahir',
+        'jenis_kelamin',
+        'alamat',
+        'no_hp',
+        'status',
+        'tanggal_daftar',
+        'tanggal_diterima',
+        'catatan',
+    ];
+
     public function siswa()
     {
-        return $this->hasOne(Siswa::class);
+        return $this->hasOne(\Modules\Siswa\Models\Siswa::class);
     }
 }
+

9c0f87817a0437335ad7d66ca3991c9683230220
muhammadafiqyunanto27 <muhammadafiqyunanto@gmail.com>
2026-04-11 11:19:06 +0700

    Fix AppServiceProvider merge conflict and code styling

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,6 +21,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        \Illuminate\Pagination\Paginator::useBootstrapFour();
+        Paginator::useBootstrapFour();
     }
 }
